5bb0e4c835355943f83f6297774373a2ec5b52b7
BT_1N3_29 <umahmood5238@bths.edu>
2018-12-10 08:41:41 -0500

    adventure

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -15,11 +15,15 @@ public class Runner {
     }
 
     public static String startMenu(){
+        String instructions = "Welcome to this Text Based Adventure. To move around, type 'North', 'South', 'East' and 'West'." +
+                " To check your location, type 'Location'. To check your inventory, type 'Inventory'. To check your health and other effects, type 'Status'." +
+                " Answer all other questions with the options provided. Your goal is to escape the pit in one piece by any means necessary. Good Luck!";
         String intro = "You wake up in a pit. You can see the sky from where you are," +
                 " but its a very long way up. It's hard to imagine surviving a fall from that height," +
                 " and yet, your body aches as if you'd fallen into the pit. To the north, there's a massive door that seems" +
-                "too heavy tp even try to open. West is another pit, no bottom to be seen. To the East, there's" +
-                "a long passage, just as dark as the pit to the West. There's a regular wooden door to the South.";
+                "too heavy to even try to open. West is another pit, no bottom to be seen. To the East, there's" +
+                "a long passage, just as dark as the pit to the West. There's a small wooden door to the South.";
+        System.out.println(instructions);
         return intro;
     }
 }
