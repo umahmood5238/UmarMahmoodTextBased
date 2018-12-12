df22dbfc0cbc6b9433c43d67bf7796221b1bb770
BT_1N3_29 <umahmood5238@bths.edu>
2018-12-12 08:40:49 -0500

    ycyty

--- src/com/company/Decisions.java
+++ src/com/company/Decisions.java
@@ -1,4 +1,6 @@
 package com.company;
+import java.util.Scanner;
 
 public class Decisions {
+
 }
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -10,18 +10,24 @@ public class Runner {
         if (start.equals("start")) {
             game = 1;
             startMenu();
+            input = new Scanner (System.in);
+            String stand = input.nextLine();
+            if (stand.equals("enter")){
+                System.out.println("What will you do?");
+            }
         }
     }
 
     public static void startMenu(){
-        String instructions = "Welcome to this Text Based Adventure. To move around, type 'North', 'South', 'East' and 'West'." +
-                " To check your location, type 'Location'. To check your inventory, type 'Inventory'. To check your health and other effects, type 'Status'." +
-                " Answer all other questions with the options provided. Your goal is to escape the pit in one piece by any means necessary. Good Luck!";
+        String instructions = "Welcome to this Text Based Adventure. To move around, type 'North', 'South', 'East' and 'West'.\n" +
+                "To check your location, type 'Location'. To check your inventory, type 'Inventory'.\nTo check your health and other effects, type 'Status'." +
+                " Answer all other questions with the options provided.\nYour goal is to escape the pit in one piece by any means necessary. Good Luck!";
         String intro = "You wake up in a pit. You can see the sky from where you are," +
                 " but its a very long way up. It's hard to imagine surviving a fall from that height," +
-                " and yet, your body aches as if you'd fallen into the pit. To the north, there's a massive door that seems" +
-                "too heavy to even try to open. West is another pit, no bottom to be seen. To the East, there's" +
-                "a long passage, just as dark as the pit to the West. There's a small wooden door to the South.";
+                " and yet, your body aches as if you'd fallen into the pit.\nTo the north, there's a massive door that seems" +
+                "too heavy to even try to open. West is another pit, no bottom to be seen.\nTo the East, there's" +
+                " a long passage, just as dark as the pit to the West. There's a small wooden door to the South.\n"
+                + "type 'enter' to stand up";
         System.out.println(instructions);
         System.out.println(intro);
     }
